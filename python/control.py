e1a5c593bdf1c5af8d35c55564b54f336e416661
Adam Heins <mail@adamheins.com>
2018-01-12 12:10:29 -0500

    Simple open-loop undamped inverted pendulum.

--- python/control.py
+++ python/control.py
@@ -7,6 +7,9 @@ A = np.eye(2)
 B = np.zeros((2, 2))
 C = np.eye(2)
 
+G = 9.81
+L = 1
+
 
 def u(t):
     ''' Control signal. '''
@@ -15,7 +18,9 @@ def u(t):
 
 def f(t, x):
     ''' State-update function: dx/dt = f(x). '''
-    return np.dot(A, x) + np.dot(B, u(t))
+    x1dot = x[1]
+    x2dot = - G / L * np.sin(x[0])
+    return np.array([x1dot, x2dot])
 
 
 def g(x):
@@ -24,18 +29,19 @@ def g(x):
 
 
 def main():
-    x0 = np.array([0, 0])
+    x0 = np.array([np.pi/2, 0])
 
-    dt = 0.1
+    # Times are in seconds.
+    dt = 0.01
     t0 = 0
-    t1 = 1
+    t1 = 5
 
     # Setup numerical integrator with 4th order Runge-Kutta solver.
     solver = integrate.ode(f).set_integrator('dopri5')
     solver.set_initial_value(x0, t0)
 
-    ts = [t0]
-    ys = [g(x0)]
+    ts = np.array([t0])
+    ys = np.array([g(x0)])
 
     # Simulate the system.
     while solver.successful() and solver.t + dt <= t1:
@@ -44,13 +50,16 @@ def main():
         y = g(x)
 
         # Record results.
-        ts.append(t)
-        ys.append(y)
+        ts = np.append(ts, t)
+        ys = np.append(ys, [y], axis=0)
 
     # Plot the results.
-    plt.plot(ts, ys)
+    plt.plot(ts, ys[:, 0], label='Angle')
+    plt.plot(ts, ys[:, 1], label='Angular velocity')
     plt.xlabel('Time (s)')
     plt.ylabel('Outputs')
+    plt.legend()
+    plt.grid()
     plt.show()
 
 
