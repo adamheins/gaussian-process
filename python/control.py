683c04eb9deeaada9523aed1668b819bde4fcf4f
Adam Heins <mail@adamheins.com>
2018-01-12 11:54:28 -0500

    Nice control simulation setup.

--- python/control.py
+++ python/control.py
@@ -1,28 +1,60 @@
 import scipy.integrate as integrate
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 A = np.eye(2)
 B = np.zeros((2, 2))
-C = np.zeros((2, 2))
+C = np.eye(2)
 
 
 def u(t):
-    ''' Control signal as a function of time. '''
+    ''' Control signal. '''
     return np.array([0, 0])
 
 
-def f(x, t):
-    ''' State-update function. '''
+def f(t, x):
+    ''' State-update function: dx/dt = f(x). '''
     return np.dot(A, x) + np.dot(B, u(t))
 
 
 def g(x):
-    ''' Output function. '''
+    ''' Output function: y = g(x) '''
     return np.dot(C, x)
 
 
-t = np.arange(0, 2, 0.1)
-x0 = np.array([0, 0])
-x = integrate.odeint(f, x0, t, args=(u,))
-print(x.shape)
+def main():
+    x0 = np.array([0, 0])
+
+    dt = 0.1
+    t0 = 0
+    t1 = 1
+
+    # Setup numerical integrator with 4th order Runge-Kutta solver.
+    solver = integrate.ode(f).set_integrator('dopri5')
+    solver.set_initial_value(x0, t0)
+
+    ts = [t0]
+    ys = [g(x0)]
+
+    # Simulate the system.
+    while solver.successful() and solver.t + dt <= t1:
+        t = solver.t + dt
+        x = solver.integrate(t)
+        y = g(x)
+
+        # Record results.
+        ts.append(t)
+        ys.append(y)
+
+    # Plot the results.
+    plt.plot(ts, ys)
+    plt.xlabel('Time (s)')
+    plt.ylabel('Outputs')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
+
+
