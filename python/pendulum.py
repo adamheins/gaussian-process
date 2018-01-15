14f997698880077d1809f58802d995d28204b4be
Adam Heins <mail@adamheins.com>
2018-01-15 13:41:02 -0500

    Add noise and disturbance signals.

--- python/pendulum.py
+++ python/pendulum.py
@@ -6,11 +6,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-grav = 9.81
-length = 1
-mass = 1
-inertia = mass * length ** 2
+GRAV = 9.81
+LENGTH = 1
+MASS = 1
+INERTIA = MASS * LENGTH ** 2
 
+# Times are in seconds.
+DT = 0.01
+T0 = 0
+TF = 10
+
+# Initial condition.
+X0 = np.array([np.pi / 2, 0])
+
+NOISE_MEAN = 0
+NOISE_SIGMA = 0.01
+DIST_MEAN = 0
+DIST_SIGMA = 1
+
+# Controller gains.
 K = np.array([1, 1])
 
 # Operating point.
@@ -22,16 +36,21 @@ def disturbance(mean, sigma):
     return np.random.normal(mean, sigma)
 
 
+def noise(mean, sigma):
+    ''' Normally distributed sensor noise. '''
+    return np.random.normal(mean, sigma)
+
+
 def control(y):
     ''' Control signal. '''
-    # Standard proportional controller.
+    # Simple PD controller.
     return -np.dot(K, y)
 
 
 def f(x, t, u):
     ''' State-update function: dx/dt = f(x). '''
     x1dot = x[1]
-    x2dot = -grav / length * np.sin(x[0]) + u
+    x2dot = -GRAV / LENGTH * np.sin(x[0]) + u
     return np.array([x1dot, x2dot])
 
 
@@ -40,26 +59,30 @@ def out(x):
     return x
 
 
-def step(x, u, d, dt):
+def step(x, u, dt):
+    ''' Simulate the system for a single timestep. '''
+    # Model uncertainty.
+    d = disturbance(DIST_MEAN, DIST_SIGMA)
+
+    # Integrate the system over a single timestep.
     x = integrate.odeint(f, x, [0, dt], args=(u + d,))
     x = x[1, :]
-    y = out(x)
+
+    # Sensor noise.
+    n = noise(NOISE_MEAN, NOISE_SIGMA)
+
+    # System output.
+    y = out(x) + n
+
     return x, y
 
 
 def main():
-    # Subtract operating point to put us into reference coordinates.
-    x0 = np.array([np.pi / 2, 0]) - REF
-
-    # Times are in seconds.
-    dt = 0.01
-    t0 = 0
-    t1 = 10
-
     # Initialize the system.
-    x = x0
-    y = out(x0)
-    t = t0
+    # Subtract operating point to put us into reference coordinates.
+    x = X0 - REF
+    y = out(x)
+    t = T0
     u = 0
 
     ts = np.array(t)
@@ -67,17 +90,14 @@ def main():
     us = np.array(u)
 
     # Simulate the system.
-    while t <= t1:
-        # Generate disturbance.
-        d = disturbance(0, 0.1)
-
+    while t <= TF:
         # Simulate the system for one time step.
-        x, y = step(x, u, d, dt)
+        x, y = step(x, u, DT)
 
         # Calculate control input for next step based on system output.
         u = control(y)
 
-        t = t + dt
+        t = t + DT
 
         # Record results.
         ts = np.append(ts, t)
@@ -88,7 +108,7 @@ def main():
     ys = ys + np.tile(REF, (ys.shape[0], 1))
 
     # Plot the results.
-    plt.plot([0, t1], [REF[0], REF[0]], label='Reference Angle (rad)')
+    plt.plot([T0, TF], [REF[0], REF[0]], label='Reference Angle (rad)')
     plt.plot(ts, ys[:, 0], label='Angle (rad)')
     plt.plot(ts, ys[:, 1], label='Angular Velocity (rad/s)')
     plt.plot(ts, us, label=u'Applied Torque (N·m)')
