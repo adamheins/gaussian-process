fd27dad1d1bd7c2822d9e9d30dda23d6de6d493f
Adam Heins <mail@adamheins.com>
2018-01-15 12:26:03 -0500

    Reformulated system to allow more flexibility.

--- python/pendulum.py
+++ python/pendulum.py
@@ -1,11 +1,14 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
 import scipy.integrate as integrate
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-grav = 9.81  # Gravity
-length = 1     # Length
-mass = 1     # Mass
+grav = 9.81
+length = 1
+mass = 1
 inertia = mass * length ** 2
 
 K = np.array([1, 1])
@@ -14,27 +17,34 @@ K = np.array([1, 1])
 REF = np.array([np.pi, 0])
 
 
-def d(mean, sigma):
+def disturbance(mean, sigma):
     ''' Normally distributed disturbance. '''
     return np.random.normal(mean, sigma)
 
 
-def u(x):
+def control(y):
     ''' Control signal. '''
     # Standard proportional controller.
-    return -np.dot(K, g(x))
+    return -np.dot(K, y)
 
 
-def f(t, x):
+def f(x, t, u):
     ''' State-update function: dx/dt = f(x). '''
     x1dot = x[1]
-    x2dot = -grav / length * np.sin(x[0])
+    x2dot = -grav / length * np.sin(x[0]) + u
     return np.array([x1dot, x2dot])
 
 
-def g(x):
+def out(x):
     ''' Output function: y = g(x) '''
     return x
+
+
+def step(x, u, d, dt):
+    x = integrate.odeint(f, x, [0, dt], args=(u + d,))
+    x = x[1, :]
+    y = out(x)
+    return x, y
 
 
 def main():
@@ -46,39 +56,45 @@ def main():
     t0 = 0
     t1 = 10
 
-    # Setup numerical integrator with 4th order Runge-Kutta solver.
-    solver = integrate.ode(f).set_integrator('dopri5', nsteps=1000)
-    solver.set_initial_value(x0, t0)
+    # Initialize the system.
+    x = x0
+    y = out(x0)
+    t = t0
+    u = 0
 
-    ts = np.array([t0])
-    ys = np.array([g(x0)])
-    us = np.array([u(x0)])
+    ts = np.array(t)
+    ys = np.array([y])
+    us = np.array(u)
 
     # Simulate the system.
-    while solver.successful() and solver.t + dt <= t1:
-        t = solver.t + dt
-        x = solver.integrate(t)
-        y = g(x)
+    while t <= t1:
+        # Generate disturbance.
+        d = disturbance(0, 0.1)
 
-        # u as function of y
-        # add disturbance
-        # set as new initial condition for the solver
-        # would also be nice to blackbox x, only get y
+        # Simulate the system for one time step.
+        x, y = step(x, u, d, dt)
+
+        # Calculate control input for next step based on system output.
+        u = control(y)
+
+        t = t + dt
 
         # Record results.
         ts = np.append(ts, t)
         ys = np.append(ys, [y], axis=0)
-        us = np.append(us, u(x))
+        us = np.append(us, u)
 
-    # Add operating point back.
+    # Add operating point back to get back to normal coordinates.
     ys = ys + np.tile(REF, (ys.shape[0], 1))
 
     # Plot the results.
-    plt.plot(ts, ys[:, 0], label='Angle')
-    plt.plot(ts, ys[:, 1], label='Angular velocity')
-    plt.plot(ts, us, label='Torque')
+    plt.plot([0, t1], [REF[0], REF[0]], label='Reference Angle (rad)')
+    plt.plot(ts, ys[:, 0], label='Angle (rad)')
+    plt.plot(ts, ys[:, 1], label='Angular Velocity (rad/s)')
+    plt.plot(ts, us, label=u'Applied Torque (N·m)')
+
     plt.xlabel('Time (s)')
-    plt.ylabel('Outputs')
+    plt.ylabel('Signals')
     plt.title('Inverted Pendulum')
     plt.legend()
     plt.grid()
