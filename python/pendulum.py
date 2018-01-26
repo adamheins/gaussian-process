67b71de0402d16f3e954abbad1b203934b16b339
Adam Heins <mail@adamheins.com>
2018-01-26 16:36:53 -0500

    Try out 3D plotting of uncertainty.

--- python/pendulum.py
+++ python/pendulum.py
@@ -8,6 +8,8 @@ import sys
 import scipy.integrate as integrate
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
 
 from gp import GaussianProcess, SEKernel
 
@@ -173,14 +175,15 @@ def main():
     plot_sigma_bounds(ts, m2s, k2s, 2, (0.8,) * 3)
     plot_sigma_bounds(ts, m2s, k2s, 1, (0.7,) * 3)
 
-    # plt.plot(ts, us, label=u'Applied Torque (N·m)')
-
     plt.xlabel('Time (s)')
     plt.ylabel('Angular velocity (rad/s)')
     plt.legend()
     plt.grid()
     plt.show()
 
+    # Plot real time vs. simulation time, so we can see how the required
+    # computation increases as more data is observed over the course of the
+    # simulation.
     plt.figure(2)
     plt.plot(ts, elapsed_time)
     plt.xlabel('Simulation time (s)')
@@ -189,7 +192,23 @@ def main():
     plt.grid()
     plt.show()
 
-    # gp.plot()
+    # Plot angle uncertainty over a portion of the state-space, with input u
+    # fixed at 0.
+    fig = plt.figure(3)
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Sample the GP across the state-space for u = 0.
+    samples = np.mgrid[-2:2:0.1, -2:2:0.1, 0:1].reshape(3, -1).T
+    m, cov = gp1.predict(samples)
+
+    # Generate plotting values.
+    X1, X2 = np.mgrid[-2:2:0.1, -2:2:0.1]
+    Z = np.sqrt(np.diag(cov)).T.reshape(40, 40)
+
+    ax.plot_surface(X1, X2, Z, cmap=cm.coolwarm, linewidth=0)
+    plt.xlabel('Angle')
+    plt.ylabel('Angular velocity')
+    plt.show()
 
 
 if __name__ == '__main__':
